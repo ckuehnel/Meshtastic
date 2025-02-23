23c655042c43049a0fc185f9a7fb41c0fb344337
Claus Kühnel <info@ckuehnel.ch>
2025-02-23 11:39:07 +0100

    MQTT Message

---
+++ MQTT/telemetry.js
@@ -0,0 +1,29 @@
+{
+  "channel": 0,
+  "from": 2692927950,
+  "hop_start": 3,
+  "hops_away": 0,
+  "id": 587303572,
+  "payload": {
+    "barometric_pressure": 0,
+    "current": -0.200000002980232,
+    "gas_resistance": 0,
+    "iaq": 0,
+    "lux": 0,
+    "radiation": 0,
+    "relative_humidity": 29.2199993133545,
+    "temperature": 24.4500007629395,
+    "voltage": 4.19999980926514,
+    "white_lux": 0,
+    "wind_direction": 0,
+    "wind_gust": 0,
+    "wind_lull": 0,
+    "wind_speed": 0
+  },
+  "rssi": -54,
+  "sender": "!fa66367c",
+  "snr": 6.75,
+  "timestamp": 1740267577,
+  "to": 4294967295,
+  "type": "telemetry"
+}
---
+++ MQTT/text.js
@@ -0,0 +1,16 @@
+{
+  "channel": 0,
+  "from": 2692927950,
+  "hop_start": 3,
+  "hops_away": 0,
+  "id": 3687588499,
+  "payload": {
+    "text": "DS18B20 Temp =  24.8 °C\n"
+  },
+  "rssi": -54,
+  "sender": "!fa66367c",
+  "snr": 6.25,
+  "timestamp": 1740267577,
+  "to": 4294967295,
+  "type": "text"
+}
